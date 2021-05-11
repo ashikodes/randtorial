617a5c1c692ec97c2fffa9367c2717f5e374cc4a
Kodi Emeka <ashikodiemeka@gmail.com>
2021-05-11 12:34:14 +0100

    remove image on landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Character from './components/Character';
-import appLogo from 'assets/images/app-logo.png';
 
 import 'scss/style.scss';
 
 function App(props) {
   return (
     <>
-      <img src={appLogo} alt="The Logo" />
       <Character />
     </>
   );
